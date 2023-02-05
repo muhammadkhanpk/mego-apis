7a07d4071648c2c91d53f3cc8e2233af00618358
Yasir Munir <38840311+muhammadkhanpk@users.noreply.github.com>
2023-02-05 15:33:16 +0500

    gcsCloud fixing

--- controllers/generalController.js
+++ controllers/generalController.js
@@ -1,5 +1,5 @@
 const { General } = require("../db/models/General");
-
+const { admin, storage } = require("../services/firebaseService");
 const saveDiameter = async (req, res) => {
   const { id, data } = req.body;
   //   return res.json(req.body);
@@ -33,4 +33,21 @@ const findDiameter = async (req, res) => {
   const d = await General.findOne({ _id: "diameter" }).lean();
   return res.json(d);
 };
-module.exports = { saveDiameter, findDiameter };
+const testingUpload = async (req, res) => {
+  let img = req.files[0];
+  try {
+    // Upload the image data to Firebase Storage
+    const file = storage.file("images/" + img.fieldname);
+    await file.save(img.buffer, {
+      metadata: {
+        contentType: "image/jpeg",
+      },
+    });
+    console.log("Image successfully uploaded to Firebase Storage.");
+    return res.json("ok");
+  } catch (error) {
+    console.error("Error uploading image: ", error);
+  }
+};
+
+module.exports = { saveDiameter, findDiameter, testingUpload };
--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -1,10 +1,13 @@
 const {
   saveDiameter,
   findDiameter,
+  testingUpload,
 } = require("../controllers/generalController");
 const express = require("express");
+const upload = require("../services/multerStorage");
 const router = express.Router();
 
 router.post("/saveDiameter", saveDiameter);
 router.get("/findDiameter", findDiameter);
+router.post("/testingUpload", upload, testingUpload);
 module.exports = router;
